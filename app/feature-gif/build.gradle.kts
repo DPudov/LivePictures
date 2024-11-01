cfaf5b59c82dfca80552e7a2553ceaf65f504946
Dmitry Pudov <dmitry.pudov@qoollo.com>
2024-11-01 23:37:43 +0300

    gifencoder and android core lib instantiated to experiment with gif encoding

--- app/feature-gif/build.gradle.kts
+++ app/feature-gif/build.gradle.kts
@@ -1,15 +1,24 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_18
+    targetCompatibility = JavaVersion.VERSION_18
 }
 
 kotlin {
     compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+        apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
+        jvmTarget.set(JvmTarget.JVM_18)
     }
+}
+
+dependencies {
+    implementation(projects.app.domain)
+    implementation(libs.core.android.lib)
+    implementation(libs.gifencoder)
 }
--- app/feature-gif/src/main/java/com/dpudov/exporter/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package com.dpudov.exporter
-
-class MyClass {
-}
