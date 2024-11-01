9e439e61ac1122141fba1ad5bf79f371dc232329
Dmitry Pudov <dmitry.pudov@qoollo.com>
2024-11-01 11:12:54 +0300

    strokes with history

--- app/src/main/java/com/dpudov/livepictures/presentation/ui/controls/LiveCanvas.kt
+++ app/src/main/java/com/dpudov/livepictures/presentation/ui/controls/LiveCanvas.kt
@@ -4,12 +4,12 @@ import android.graphics.Bitmap
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffXfermode
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -17,24 +17,18 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.BlendMode
-import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.Paint
-import androidx.compose.ui.graphics.PaintingStyle
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.input.pointer.PointerInputChange
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
 import com.dpudov.domain.model.Frame
 import com.dpudov.domain.model.Instrument
 import com.dpudov.domain.model.Point
@@ -57,25 +51,36 @@ fun LiveCanvas(
     color: Int = StandardColor.WHITE,
     modifier: Modifier = Modifier
 ) {
-    val context = LocalContext.current
-    val backgroundBitmap = ImageBitmap.imageResource(context.resources, R.drawable.paper_texture)
+//    val context = LocalContext.current
+//    val backgroundBitmap = ImageBitmap.imageResource(context.resources, R.drawable.paper_texture)
 
     var currentStroke by remember { mutableStateOf<Stroke?>(null) }
-    val strokePathCache = remember {
-        Bitmap.createBitmap(1080, 1920, Bitmap.Config.ARGB_8888)
-            .apply {
+
+    var canvasSize by remember { mutableStateOf(IntSize.Zero) }
+    val transparentBitmap = remember(canvasSize) {
+        if (canvasSize.width > 0 && canvasSize.height > 0) {
+            Bitmap.createBitmap(canvasSize.width, canvasSize.height, Bitmap.Config.ARGB_8888).apply {
                 eraseColor(android.graphics.Color.TRANSPARENT)
             }
-    }
-    LaunchedEffect(frame, strokes, currentStroke) {
-        drawStrokesToBitmap(strokePathCache, strokes)
+        } else {
+            null
+        }
     }
 
     Box(modifier = modifier) {
         if (frame != null) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop,
+                painter = painterResource(R.drawable.paper_texture),
+                contentDescription = stringResource(R.string.canvas_background)
+            )
             Canvas(modifier = Modifier
                 .fillMaxSize()
-                .pointerInput(frame, strokePathCache, instrument, color) {
+                .onSizeChanged { size ->
+                    canvasSize = size
+                }
+                .pointerInput(frame, strokes, instrument, color) {
                     detectDragGestures(
                         onDragStart = { offset: Offset ->
                             val id = UUID.randomUUID()
@@ -118,36 +123,17 @@ fun LiveCanvas(
                     )
                 }
             ) {
-                drawImage(backgroundBitmap)
-
-                drawIntoCanvas { canvas: Canvas ->
-                    canvas.nativeCanvas.drawBitmap(
-                        strokePathCache,
-                        0f,
-                        0f,
-                        null
-                    )
-                }
-
-                currentStroke?.let { stroke ->
-                    val paint = Paint().apply {
-                        this.color =
-                            if (stroke.instrument == Instrument.Eraser) Color.Transparent
-                            else Color(stroke.color)
-                        blendMode =
-                            if (stroke.instrument == Instrument.Eraser) BlendMode.Clear
-                            else BlendMode.SrcOver
-                        strokeWidth = stroke.thickness
-                        style = PaintingStyle.Stroke
-                        strokeCap = StrokeCap.Round
+                transparentBitmap?.let { bitmap: Bitmap ->
+                    val canvas = android.graphics.Canvas(bitmap)
+                    canvas.drawColor(android.graphics.Color.TRANSPARENT, PorterDuff.Mode.CLEAR)
+                    strokes.forEach {
+                        canvas.drawStroke(it)
                     }
-
-                    drawIntoCanvas { canvas ->
-                        val path = Path().apply {
-                            moveTo(stroke.points.first().x, stroke.points.first().y)
-                            stroke.points.drop(1).forEach { lineTo(it.x, it.y) }
-                        }
-                        canvas.drawPath(path, paint)
+                    currentStroke?.let {
+                        canvas.drawStroke(it)
+                    }
+                    drawIntoCanvas {
+                        it.nativeCanvas.drawBitmap(bitmap, 0f, 0f, null)
                     }
                 }
             }
@@ -161,49 +147,24 @@ fun LiveCanvas(
     }
 }
 
-fun DrawScope.drawStroke(stroke: Stroke) {
+fun android.graphics.Canvas.drawStroke(stroke: Stroke) {
     if (stroke.points.isEmpty()) return
-    val path = Path().apply {
+    val path = android.graphics.Path().apply {
         moveTo(stroke.points.first().x, stroke.points.first().y)
         for (point in stroke.points.drop(1)) {
             lineTo(point.x, point.y)
         }
     }
-
-    drawPath(
-        path = path,
-        color = Color(stroke.color),
-        style = androidx.compose.ui.graphics.drawscope.Stroke(
-            width = stroke.thickness,
-            cap = StrokeCap.Round
-        ),
-        blendMode = if (stroke.instrument == Instrument.Eraser) BlendMode.Clear
-        else BlendMode.SrcOver
-    )
-}
-
-fun drawStrokesToBitmap(bitmap: Bitmap, strokes: List<Stroke>) {
-    val canvas = android.graphics.Canvas(bitmap)
-    val paint = android.graphics.Paint()
-    canvas.drawColor(android.graphics.Color.TRANSPARENT, PorterDuff.Mode.CLEAR)
-
-    strokes.forEach { stroke ->
-        paint.apply {
-            color =
-                if (stroke.instrument == Instrument.Eraser) android.graphics.Color.TRANSPARENT
-                else stroke.color
-            xfermode =
-                if (stroke.instrument == Instrument.Eraser) PorterDuffXfermode(PorterDuff.Mode.CLEAR)
-                else null
-            style = android.graphics.Paint.Style.STROKE
-            strokeCap = android.graphics.Paint.Cap.ROUND
-            strokeWidth = stroke.thickness
-        }
-
-        val path = android.graphics.Path().apply {
-            moveTo(stroke.points.first().x, stroke.points.first().y)
-            stroke.points.drop(1).forEach { lineTo(it.x, it.y) }
-        }
-        canvas.drawPath(path, paint)
+    val paint = android.graphics.Paint().apply {
+        this.color =
+            if (stroke.instrument == Instrument.Eraser) android.graphics.Color.TRANSPARENT
+            else stroke.color
+        xfermode =
+            if (stroke.instrument == Instrument.Eraser) PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+            else null
+        strokeWidth = stroke.thickness
+        style = android.graphics.Paint.Style.STROKE
+        strokeCap = android.graphics.Paint.Cap.ROUND
     }
+    drawPath(path, paint)
 }
